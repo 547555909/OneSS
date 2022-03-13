e5d846741ec505bd8ae90c44107108ee13d9260b
uncle.mario <547555909@qq.com>
2022-03-13 14:37:46 +0800

    Create giscusSetting.ts

---
+++ setting/giscusSetting.ts
@@ -0,0 +1,10 @@
+const giscus = {
+    enabled: true,
+    repo: "skyworldp/OneSS",
+    repo_id: "R_kgDOG0nZOg",
+    category: "Announcements",
+    category_id: "DIC_kwDOG0nZOs4CBGa0",
+    term: "any"
+}
+
+export default giscus
